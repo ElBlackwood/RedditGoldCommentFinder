6f97896c9744a5c215d8ebe42acf1bcb6f5854de
Luke <lukeblackwood@gmail.com>
2013-05-28 22:25:23 +0100

    Adding log4j.xml and launch bat

---
+++ Reddit Gold Comment Finder/Reddit Gold Counter/src/RunGoldFinder.bat
@@ -0,0 +1,4 @@
+echo See output folder for log file(recommend SnakeTail for viewing it).
+echo When process is finished a .txt file of results will appear in the output folder.
+
+java -jar RedditGoldCommentFinder.jar
